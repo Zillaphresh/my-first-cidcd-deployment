0f753fc3f6084c4541f362fd54bf1b28abd14d18
godwin-letiarts <godwin@letiarts.com>
2022-07-25 12:26:18 +0000

    added new ip address

--- roles/prepare/files/index.js
+++ roles/prepare/files/index.js
@@ -1,6 +1,6 @@
 var http = require("http");
 var server = http.createServer(function (req, res) {
 res.writeHead(200);
-res.sendFile("index.html");
+res.end("Hello world!");
 });
 server.listen(3000);
